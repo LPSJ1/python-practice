372cd347fb5e089b31af7e398493203defcd158d
Vic <victormwakisagu@gmail.com>
2025-04-26 02:49:57 +0300

    sorting

---
+++ Qn_3/factorial_que.py
@@ -0,0 +1,9 @@
+def factorial():
+    number = int(input("Enter numero(number):"))
+
+    factorial = 1
+
+    for n in range(1, number + 1):
+        result *= n
+
+    print(f"The factorial of {number} is {result}.")    
